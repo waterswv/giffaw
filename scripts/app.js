d01b0fce003fd8569ef9808b357b64de4a3e4069
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-06 18:06:30 -0700

    added js to query giphy api

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
 $(document).on("ready", function(){
 
+  $('.form-inline').on("submit", function (event){
+
+    event.preventDefault();
+    console.log("something is happening")
+    $.ajax({
+
+      method: "get",
+      url: "https://api.giphy.com/v1/gifs/search"+ "?"+$(this).serialize(),
+      dataType: "json",
+      success: onSuccess
+
+    });
+
+  });
+
 });
+
+//var myString = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=cats&limit=25&offset=5&rating=g&lang=en&fmt=json";
+
+function onSuccess (){
+  console.log("you made the call correctly")
+}
